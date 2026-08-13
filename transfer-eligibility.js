03044290e6b00aa6ddf9eac22d49c9d49989d202
thomaswrixon <thomas.wrixon@gmail.com>
2026-08-13 18:13:51 +1000

    Recognise natural explicit family transfer requests

--- transfer-eligibility.js
+++ transfer-eligibility.js
@@ -10,7 +10,9 @@ function decideTransferEligibility({ callerText = "", isFriendsFamily = false } 
 
   const negativeUrgency = /(not an emergency|non[- ]?emergency|not urgent|no rush|safe for now|can wait|when he gets a chance|not sure if i need him right now|not sure.*need him.*right now|just let him know|i will try him later|i'll try him later)/i.test(text);
 
-  const explicitTry = /(can|could|would|will) you (?:please )?(?:try|get|reach|call) (?:tommy|dad|him)|please (?:try|get|reach|call) (?:tommy|dad|him)|try to get (?:tommy|dad|him)|put me (?:straight )?through|can i speak (?:to|with) tommy|need to speak (?:to|with) tommy (?:now|urgently|immediately)/i.test(text);
+  // Explicit transfer intent includes natural family wording such as
+  // "can you see if you can get Dad?" as well as direct "try Tommy" requests.
+  const explicitTry = /(?:can|could|would|will) you (?:please )?(?:see if you can |see if you could )?(?:try|get|reach|call) (?:tommy|dad|him)|please (?:see if you can |see if you could )?(?:try|get|reach|call) (?:tommy|dad|him)|try to get (?:tommy|dad|him)|put me (?:straight )?through|can i speak (?:to|with) tommy|need to speak (?:to|with) tommy (?:now|urgently|immediately)/i.test(text);
 
   const urgentMatter = /(urgent|urgently|immediate|immediately|as soon as possible|asap|needs? (?:him|tommy|a parent) now|parent.*pickup|organise pickup|water pouring|still running.*water|active leak|before (?:one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|\d)(?:\s|:|\.)|until (?:one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|\d).*today|courier cutoff|cutoff is|cutoff.*(?:today|pm|am)|miss today's lodgement|miss today'?s lodgement)/i.test(text);
 
